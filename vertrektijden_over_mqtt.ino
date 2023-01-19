407caaf6216cd85ca4882e38e269068758313a76
Max Osewoudt <info@maxosewoudt.nl>
2023-01-19 21:40:34 +0100

    Removed tft-code

--- vertrektijden_over_mqtt.ino
+++ vertrektijden_over_mqtt.ino
@@ -1,20 +1,12 @@
 #include <WiFi.h>
 #include <PubSubClient.h>
 #include <Wire.h>
-#include <TFT_eSPI.h>
-#include "MyFont.h"
 #include <NTPClient.h>
 #include <WiFiUDP.h>
 #include <ESPAsyncWebServer.h>
 #include "index_html.h"
 #include "success_html.h"
 #include "secrets.h"
-
-#define BIGFONT &FreeMonoBold24pt7b
-#define MIDBIGFONT &FreeMonoBold18pt7b
-#define MIDFONT &FreeMonoBold12pt7b
-#define SMALLFONT &FreeMonoBold9pt7b
-#define TINYFONT &TomThumb
 
 // Replace the next variables with your SSID/Password combination
 // const char* ssid = WIFI_SSID;
@@ -32,7 +24,6 @@ String dir_two;
 const char* mqtt_server = MQTT_SERVER_HOST;
 WiFiClient espClient;
 PubSubClient client(espClient);
-TFT_eSPI display = TFT_eSPI(135, 240);
 
 WiFiUDP ntpUDP;
 NTPClient timeClient(ntpUDP);
@@ -60,13 +51,6 @@ void setup_wifi() {
 
   server.begin();
   while (WiFi.status() != WL_CONNECTED) {
-    display.begin();
-    display.setRotation(1);
-    display.fillScreen(TFT_BLACK);
-    display.setCursor(0, 20);
-    display.setTextColor(TFT_YELLOW, TFT_YELLOW);
-    display.setFreeFont(SMALLFONT);
-    display.println("Waiting for wifi...");
     delay(1000);
     Serial.println("Waiting for wifi credentials...");
   }
@@ -105,20 +89,12 @@ void callback(char* topic, byte* message, unsigned int length) {
     Serial.print((char)message[i]);
     messageTemp += (char)message[i];
   }
-  print_on_builtin_display(messageTemp);
+  print_on_matrix(messageTemp);
   Serial.println();
 }
 
-void print_on_builtin_display(String departure)
+void print_on_matrix(String departure)
 {
-  display.fillScreen(TFT_BLACK);
-  display.setCursor(0, 20);
-  display.setTextColor(TFT_YELLOW, TFT_YELLOW);
-  display.setFreeFont(SMALLFONT);
-  display.println("L Bestemming       V");
-  display.setFreeFont(MIDFONT);
-  display.setTextColor(TFT_WHITE, TFT_BLACK);
-
   // Split the string into an array of substrings
   int index = departure.indexOf(delimiter);
   if (index != -1) {
@@ -129,20 +105,15 @@ void print_on_builtin_display(String departure)
     dir_one = departure;
     dir_two = "";
   }
-  display.println(dir_one);
-  display.println("-----------------");
-  display.println(dir_two);
-  display.println("-----------------");
-  display.println(getCurrentTime());
 }
 
 String getCurrentTime() {
-    time_t now = timeClient.getEpochTime();
-    struct tm * timeinfo;
-    char buffer[80];
-    timeinfo = gmtime(&now);
-    strftime(buffer, sizeof(buffer), "%d-%m-%y %H:%M:%S", timeinfo);
-    return buffer;
+  time_t now = timeClient.getEpochTime();
+  struct tm * timeinfo;
+  char buffer[80];
+  timeinfo = gmtime(&now);
+  strftime(buffer, sizeof(buffer), "%d-%m-%y %H:%M:%S", timeinfo);
+  return buffer;
 }
 
 void setup() {
